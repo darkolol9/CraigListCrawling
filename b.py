94a6f586184b5ae3db26aecdcf2cdac5ae00bb4a
darkolol9 <53996212+darkolol9@users.noreply.github.com>
2021-12-22 10:31:49 +0200

    edited vis

--- b.py
+++ b.py
@@ -3,6 +3,21 @@ import pandas as pd
 
 
 df = pd.read_csv('listings.csv')
+df = df.drop_duplicates()
+df.shape
+
+df['Price'] = df['Price'].str.replace('$','').astype(int)
+
 
 df.tail()
+# %%
+# grp = df.groupby('Paint color').count()
+# grp.plot(kind='pie')
+
+colors = df.groupby('Transmission')['Model'].mean()
+
+colors.plot(kind='bar')
+
+
+
 # %%
--- selen.py
+++ selen.py
@@ -6,8 +6,7 @@ from bs4 import BeautifulSoup
 
 driver = webdriver.Chrome()
 
-url = 'https://atlanta.craigslist.org/search/sss?lat=33.14106781674328&lon=-84.14804723122097&sort=date&purveyor-input=all&search_distance=250&condition=40&condition=50&condition=60&auto_bodytype=1&auto_bodytype=2&auto_bodytype=3&auto_bodytype=4&auto_bodytype=5&auto_bodytype=6&auto_bodytype=7&auto_bodytype=8&auto_bodytype=9&auto_bodytype=10&auto_bodytype=11&auto_bodytype=12'
-
+url = 'https://kansascity.craigslist.org/search/sss?auto_bodytype=1&auto_bodytype=10&auto_bodytype=11&auto_bodytype=12&auto_bodytype=2&auto_bodytype=3&auto_bodytype=4&auto_bodytype=5&auto_bodytype=6&auto_bodytype=7&auto_bodytype=8&auto_bodytype=9&condition=40&condition=50&condition=60&lat=38.75752717346409&lon=-94.5347186248444&purveyor-input=all&search_distance=250&sort=date'
 driver.get(url)
 
 first_title = driver.find_element_by_class_name('result-heading')
@@ -17,14 +16,19 @@ first_title.click()
 stats = {}
 
 
-with open('listings.csv','w') as f:
-    f.write('Model,Brand,Price,Odometer,Paint color,Condition,Fuel,Transmission,Type\n')
-    for _ in range(10):
-        page = driver.page_source
-        soup = BeautifulSoup(page,'html.parser')
-        price  = soup.find('span',class_='price').text.replace(',','')
-        attrs = soup.find_all('p',class_='attrgroup')
-        type = attrs[0].find('span').text
+with open('listings.csv','a') as f:
+    # f.write('Model,Brand,Price,Odometer,Paint color,Condition,Fuel,Transmission,Type\n')
+    while True:
+        print('crawling...')
+
+        try:
+            page = driver.page_source
+            soup = BeautifulSoup(page,'html.parser')
+            price  = soup.find('span',class_='price').text.replace(',','')
+            attrs = soup.find_all('p',class_='attrgroup')
+            type = attrs[0].find('span').text
+        except:
+            pass
 
         stats = {'model': type.split()[0],
         'brand':type.split()[1]
@@ -36,7 +40,10 @@ with open('listings.csv','w') as f:
         
 
         for sp in spans:
-            stats[sp.text[:sp.text.find(':')]] = sp.find('b').text
+            try:
+                stats[sp.text[:sp.text.find(':')]] = sp.find('b').text
+            except:
+                pass
 
         
         
@@ -48,8 +55,10 @@ with open('listings.csv','w') as f:
          
 
 
-
-        driver.find_element_by_class_name('next').click() 
+        try:
+            driver.find_element_by_class_name('next').click() 
+        except:
+            driver.refresh()
     
     driver.close()
 
