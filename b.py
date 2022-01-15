93025eaf2ce0a6017145f7769cc5f2ecb0dd1a49
darkolol9 <53996212+darkolol9@users.noreply.github.com>
2022-01-15 14:24:44 +0200

    added confusion matrix

--- b.py
+++ b.py
@@ -3,6 +3,8 @@ from numpy.core.numerictypes import ScalarType
 import pandas as pd
 from scipy import stats
 import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 
 # df = pd.read_csv('listings.csv')
@@ -10,8 +12,8 @@ df = pd.read_csv('listings_no_dupe.csv')
 # df = df.drop_duplicates()
 
 # df['Price'] = df['Price'].str.replace('$','').astype(float)
+df = df[:1000]
 df.shape
-
 # df.to_csv('listings_no_dupe.csv',index=False)
 
 # df.tail()
@@ -20,7 +22,7 @@ df.shape
 # %% 
 
 def remove_outlier(df_in, col_name):
-    q1 = df_in[col_name].quantile(0.25)
+    q1 = df_in[col_name].quantile(0.40)
     q3 = df_in[col_name].quantile(0.75)
     iqr = q3-q1 #Interquartile range
     fence_low  = q1-1.5*iqr
@@ -38,14 +40,22 @@ for i,row in enumerate(df['Brand']):
 
 df.Brand = df.Brand.str.upper()
 
-df = df.drop(df[(df['Brand'] == 'null')].index)
-df = df[df.Price > 100]
+df = df.drop(df[(df['Brand'] == 'NULL')].index)
+df = df[df.Price > 500]
 
 
 df =  remove_outlier(df,'Price')
 
 
-# df.Price.describe()
+
+plt.scatter(df.Model,df.Price,c='red')
+# plt.plot(df.Price)
+
+
+
+# df.describe()
+
+
 
 
 
@@ -98,6 +108,8 @@ le = labelEncoder()
 le.fit(df)
 le.transform(df)
 
+
+
 X,y = split_x_y(df)
 
 # 2015,Acura,22990.0,32917,black,good,other,other,sedan
@@ -111,7 +123,20 @@ data = pd.DataFrame(data)
 
 
 
+
+
 data = le.transform(data)
+
+
+plt.figure(figsize=(12,10))
+cor = df.corr()
+sns.heatmap(cor, annot=True, cmap=plt.cm.Reds)
+plt.show()
+
+cor_target = abs(cor["Price"])
+#Selecting highly correlated features
+relevant_features = cor_target[cor_target>0.1]
+print(relevant_features)
 
 
 
@@ -122,26 +147,26 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 
 
-svr = SVR(kernel='rbf').fit(X_train,y_train)
+# svr = SVR(kernel='rbf').fit(X_train,y_train)
 regressor = RandomForestRegressor(random_state=0).fit(X_train,y_train)
-sgd = SGDRegressor().fit(X_train,y_train)
-en = ElasticNet().fit(X_train,y_train)
-br = BayesianRidge().fit(X_train,y_train)
-lr = LinearRegression().fit(X_train,y_train)
+# sgd = SGDRegressor().fit(X_train,y_train)
+# en = ElasticNet().fit(X_train,y_train)
+# br = BayesianRidge().fit(X_train,y_train)
+# lr = LinearRegression().fit(X_train,y_train)
 
-param = {'n_estimators':[i for i in range(100,102)],'max_depth':[i for i in range(10)]}
-cv = GridSearchCV(regressor,param,scoring='r2')
-cv.fit(X_train,y_train)
+# param = {'n_estimators':[i for i in range(100,102)],'max_depth':[i for i in range(10)]}
+# cv = GridSearchCV(regressor,param,scoring='r2')
+# cv.fit(X_train,y_train)
 
-print(cv.best_params_,cv.best_score_)
+# print(cv.best_params_,cv.best_score_)
 
 
 ypred_rf  = regressor.predict(X_test)
-ypred_svr  = svr.predict(X_test)
-ypred_sgd  = sgd.predict(X_test)
-ypred_en = en.predict(X_test)
-ypred_br = br.predict(X_test)
-ypred_lr = lr.predict(X_test)
+# ypred_svr  = svr.predict(X_test)
+# ypred_sgd  = sgd.predict(X_test)
+# ypred_en = en.predict(X_test)
+# ypred_br = br.predict(X_test)
+# ypred_lr = lr.predict(X_test)
 
 # ypred = regressor.predict(np.array(data))
 
@@ -153,13 +178,15 @@ ypred_lr = lr.predict(X_test)
 
 
 # regressor.score(X,y)
-# print(metrics.r2_score(y_test,ypred_rf),' random forrest score')
+print(metrics.r2_score(y_test,ypred_rf),' random forrest score')
 # print(metrics.r2_score(y_test,ypred_svr),' svr score')
 # print(metrics.r2_score(y_test,ypred_sgd),' sgd score')
 # print(metrics.r2_score(y_test,ypred_en),' elastic net score')
 # print(metrics.r2_score(y_test,ypred_br),' bayessian  score')
 # print(metrics.r2_score(y_test,ypred_lr),' linear model  score')
 
+# print(regressor.feature_names_in_)
+# regressor.feature_importances_
 
 
 # %%
--- bot.py
+++ bot.py
@@ -3,15 +3,61 @@ import pickle
 import discord
 import pandas as pd
 import numpy as np
+import re
+from bs4 import BeautifulSoup
+import requests
 
 
 
 TOKEN = 'OTI5NDY3NDIzMzA2OTU2ODEw.Ydnv_g.GAN1pP9F_iFWIJbvtEr1Qg2ks8A'
 
-encoder = pickle.load(open('labelEncoder.pkl','rb'))
+encoder = pickle.load(open('encoder.pkl','rb'))
 model = pickle.load(open('randomForest.pkl','rb'))
 
 
+def url_to_data_dict(url:str) -> dict:
+  data = {'Model':0,
+          'Brand':0,
+          'Odometer':0,
+          'Paint color':0,
+          'Condition':0,
+          'Fuel':0,
+          'Transmission':0,
+          'Type':0}
+
+
+
+
+
+  stats = {}
+
+  soup = BeautifulSoup(requests.get(url).content,'lxml')
+
+  headline = soup.find('span',attrs={'id':'titletextonly'})
+
+  data['Model'] = re.findall('\d\d\d\d',headline.text)[0]
+  data['Brand'] = re.findall(' ([A-Za-z]+)',headline.text)[0].upper()
+
+  features = soup.find_all('p',class_='attrgroup')[1]
+
+
+  for sp in features:
+    try:
+      stats[sp.text[:sp.text.find(':')]] = sp.find('b').text
+    except:
+      pass
+
+
+
+  for key in stats:
+    if key.title() in data:
+      data[key.title()] = stats[key]
+
+    if key == 'paint color':
+      data['Paint color'] = stats[key]
+
+
+  return str(data)
 
 
 def preprocess_data_and_predict(data,model,encoder):
@@ -37,9 +83,11 @@ async def on_message(message):
     if message.content.startswith('check'):
         channel = message.channel
         k = message.content
-        data = eval(k[5:])
-        res = (preprocess_data_and_predict(data,model,encoder))
+        data = k[5:]
+        data = url_to_data_dict(data)
+        res = (preprocess_data_and_predict(eval(data),model,encoder))
         await channel.send(f'*Best i can do* for this car is *slaps rooftop* : {round(res)}$```py\n{data}```')
+        # await channel.send(data)
     
     if message.content.startswith('score'):
         msg = f'```py\n r2_score={82}%```'
